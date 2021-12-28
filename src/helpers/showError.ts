6968d7463037a18f008ae56337aa56941a0ba321
Kingkor Roy Tirtho <krtirtho@gmail.com>
2021-12-28 23:06:50 +0600

    removed docker support

--- src/helpers/showError.ts
+++
@@ -1,8 +0,0 @@
-import { trace } from "../conf";
-import chalk from "chalk";
-
-function showError(error: Error | TypeError, message = "[Error]: ") {
-    console.error(chalk.red(message), trace ? error : error.message);
-}
-
-export default showError;
