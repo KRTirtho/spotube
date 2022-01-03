3068b4ca4c560372385b4922e9e850009f26cb24
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-01-03 10:13:32 +0600

    All react-nodegui junk cleaned up

--- src/hooks/useLogger.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from "react";
-import { Logger } from "../initializations/logger";
-
-export function useLogger(module: string) {
-    return useMemo(() => new Logger(module), []);
-}
