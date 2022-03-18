a4b2f0673776828a83bbda06679ae0e1965681f3
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-03-18 13:05:55 +0600

    create-secrets is now argv based

--- bin/create-secrets.dart
+++ bin/create-secrets.dart
@@ -3,17 +3,18 @@ import 'dart:io';
 
 import 'package:path/path.dart' as path;
 
-void main() async {
-  final env = Platform.environment;
-  final bool hasKey = env.containsKey("SECRET");
-  final val = hasKey ? jsonDecode(env["SECRET"]!) : null;
-  print("SECRET VALUE: ${env["SECRET"]}");
-  if (!hasKey || (hasKey && val is! List)) {
+void main(List<String> args) async {
+  if (args.isEmpty) {
+    throw ArgumentError("Expected an argument but none was passed");
+  }
+
+  final val = jsonDecode(args.first);
+  if (val is! List) {
     throw Exception(
         "'SECRET' Environmental Variable isn't configured properly");
   }
 
   await File(path.join(
           Directory.current.path, "lib/models/generated_secrets.dart"))
-      .writeAsString("final List<String> secrets = ${env["SECRET"]};");
+      .writeAsString("final List<String> secrets = ${args.first};");
 }
