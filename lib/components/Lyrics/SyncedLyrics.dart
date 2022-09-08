531fae64f94b21551a7a0da363a9ab0d44f5d3b1
Kingkor Roy Tirtho <krtirtho@gmail.com>
2022-09-08 23:28:16 +0600

    feat(synced-lyrics): animated active text size

--- lib/components/Lyrics/SyncedLyrics.dart
+++ lib/components/Lyrics/SyncedLyrics.dart
@@ -221,22 +221,23 @@ class SyncedLyrics extends HookConsumerWidget {
                                       : Center(
                                           child: Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: AutoSizeText(
-                                              lyricSlice.text,
-                                              maxLines: 2,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .headline4
-                                                  ?.copyWith(
-                                                    color: isActive
-                                                        ? Colors.white
-                                                        : palette.bodyTextColor,
-                                                    // indicating the active state of that lyric slice
-                                                    fontWeight: isActive
-                                                        ? FontWeight.bold
-                                                        : null,
-                                                  ),
-                                              textAlign: TextAlign.center,
+                                            child: AnimatedDefaultTextStyle(
+                                              duration: const Duration(
+                                                  milliseconds: 250),
+                                              style: TextStyle(
+                                                color: isActive
+                                                    ? Colors.white
+                                                    : palette.bodyTextColor,
+                                                fontWeight: isActive
+                                                    ? FontWeight.bold
+                                                    : FontWeight.normal,
+                                                fontSize: isActive ? 30 : 26,
+                                              ),
+                                              child: Text(
+                                                lyricSlice.text,
+                                                maxLines: 2,
+                                                textAlign: TextAlign.center,
+                                              ),
                                             ),
                                           ),
                                         ),
