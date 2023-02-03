1956692484761993508d6d56316dbe22909f6b4b
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-02-03 17:10:41 +0600

    cd: add hosted server configurations in workflow

--- lib/collections/env.dart
+++ lib/collections/env.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract class Env {
-  static final String pocketbaseUrl = dotenv.get('POCKETBASE_URL');
-  static final String username = dotenv.get('USERNAME');
-  static final String password = dotenv.get('PASSWORD');
+  static final String pocketbaseUrl =
+      dotenv.get('POCKETBASE_URL', fallback: 'http://localhost:8090');
+  static final String username = dotenv.get('USERNAME', fallback: 'root');
+  static final String password = dotenv.get('PASSWORD', fallback: '12345678');
 
   static configure() async {
-    await dotenv.load(fileName: ".env");
+    if (kReleaseMode) {
+      await dotenv.load(fileName: ".env");
+    }
   }
 }
