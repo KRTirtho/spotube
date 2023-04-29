6d836bdb658c180ca8e2c71e7e290fafa3520727
Kingkor Roy Tirtho <kingkor.tirtho@leam.ae>
2023-04-29 15:31:42 +0600

    feat(translation): add hindi and french translations using ChatGPT

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,14 @@
+/// credits:
+///
+/// Kingkor Roy Tirtho => English, Bengali
+/// ChatGPT (GPT 3.5) XD => Hindi, French
 import 'package:flutter/material.dart';
 
 class L10n {
   static final all = [
     const Locale('en'),
     const Locale('bn', 'BD'),
+    const Locale('fr', 'FR'),
+    const Locale('hi', 'IN'),
   ];
 }
