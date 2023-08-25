d239d641ff8f1b3edd64243994fd4a58cf71a5d3
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-08-25 18:54:54 +0600

    feat: paginated user albums

--- lib/components/library/user_albums.dart
+++ lib/components/library/user_albums.dart
@@ -3,13 +3,14 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:collection/collection.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:spotify/spotify.dart';
 
 import 'package:spotube/collections/spotube_icons.dart';
 import 'package:spotube/components/album/album_card.dart';
 import 'package:spotube/components/shared/shimmers/shimmer_playbutton_card.dart';
 import 'package:spotube/components/shared/fallbacks/anonymous_fallback.dart';
+import 'package:spotube/components/shared/waypoint.dart';
 import 'package:spotube/extensions/context.dart';
-import 'package:spotube/hooks/use_breakpoint_value.dart';
 import 'package:spotube/provider/authentication_provider.dart';
 import 'package:spotube/services/queries/queries.dart';
 
@@ -23,76 +24,86 @@ class UserAlbums extends HookConsumerWidget {
     final auth = ref.watch(AuthenticationNotifier.provider);
     final albumsQuery = useQueries.album.ofMine(ref);
 
-    final spacing = useBreakpointValue<double>(
-      xs: 0,
-      sm: 0,
-      others: 20,
-    );
+    final controller = useScrollController();
 
     final searchText = useState('');
 
+    final allAlbums = useMemoized(
+      () => albumsQuery.pages
+          .expand((element) => element.items ?? <AlbumSimple>[]),
+      [albumsQuery.pages],
+    );
+
     final albums = useMemoized(() {
       if (searchText.value.isEmpty) {
-        return albumsQuery.data?.toList() ?? [];
+        return allAlbums;
       }
-      return albumsQuery.data
-              ?.map((e) => (
-                    weightedRatio(e.name!, searchText.value),
-                    e,
-                  ))
-              .sorted((a, b) => b.$1.compareTo(a.$1))
-              .where((e) => e.$1 > 50)
-              .map((e) => e.$2)
-              .toList() ??
-          [];
-    }, [albumsQuery.data, searchText.value]);
+      return allAlbums
+          .map((e) => (
+                weightedRatio(e.name!, searchText.value),
+                e,
+              ))
+          .sorted((a, b) => b.$1.compareTo(a.$1))
+          .where((e) => e.$1 > 50)
+          .map((e) => e.$2)
+          .toList();
+    }, [allAlbums, searchText.value]);
 
     if (auth == null) {
       return const AnonymousFallback();
     }
 
+    final theme = Theme.of(context);
+
     return RefreshIndicator(
       onRefresh: () async {
         await albumsQuery.refresh();
       },
-      child: SingleChildScrollView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SearchBar(
+      child: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ColoredBox(
+                color: theme.scaffoldBackgroundColor,
+                child: SearchBar(
                   onChanged: (value) => searchText.value = value,
                   leading: const Icon(SpotubeIcons.filter),
-                  hintText: context.l10n.filter_albums,
+                  hintText: context.l10n.filter_artist,
                 ),
-                const SizedBox(height: 20),
-                AnimatedCrossFade(
-                  duration: const Duration(milliseconds: 300),
-                  firstChild: Container(
-                    alignment: Alignment.topLeft,
-                    padding: const EdgeInsets.all(16.0),
-                    child: const ShimmerPlaybuttonCard(count: 7),
-                  ),
-                  secondChild: Wrap(
-                    spacing: spacing, // gap between adjacent chips
-                    runSpacing: 20, // gap between lines
-                    alignment: WrapAlignment.center,
-                    children: albums
-                        .map((album) => AlbumCard(
-                              TypeConversionUtils.simpleAlbum_X_Album(album),
-                            ))
-                        .toList(),
-                  ),
-                  crossFadeState: albumsQuery.isLoading ||
-                          !albumsQuery.hasData ||
-                          searchText.value.isNotEmpty
-                      ? CrossFadeState.showFirst
-                      : CrossFadeState.showSecond,
-                ),
-              ],
+              ),
+            ),
+          ),
+          body: SizedBox.expand(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(8.0),
+              controller: controller,
+              child: Wrap(
+                runSpacing: 20,
+                alignment: WrapAlignment.center,
+                runAlignment: WrapAlignment.center,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  if (albums.isEmpty)
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.all(16.0),
+                      child: const ShimmerPlaybuttonCard(count: 4),
+                    ),
+                  for (final album in albums)
+                    AlbumCard(
+                      TypeConversionUtils.simpleAlbum_X_Album(album),
+                    ),
+                  if (albumsQuery.hasNextPage)
+                    Waypoint(
+                      controller: controller,
+                      isGrid: true,
+                      onTouchEdge: albumsQuery.fetchNext,
+                      child: const ShimmerPlaybuttonCard(count: 1),
+                    )
+                ],
+              ),
             ),
           ),
         ),
