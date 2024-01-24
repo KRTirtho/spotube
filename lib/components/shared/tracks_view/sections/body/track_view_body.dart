624220090572eb643dce37ca8ffd85d2b3f5c9df
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-01-24 12:29:35 +0600

    feat: haptic feedback on long press and reordering actions

--- lib/components/shared/tracks_view/sections/body/track_view_body.dart
+++ lib/components/shared/tracks_view/sections/body/track_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:gap/gap.dart';
@@ -116,6 +117,7 @@ class TrackViewBodySection extends HookConsumerWidget {
                       },
                 onLongPress: () {
                   trackViewState.selectTrack(track.id!);
+                  HapticFeedback.selectionClick();
                 },
                 onTap: () async {
                   if (trackViewState.isSelecting) {
