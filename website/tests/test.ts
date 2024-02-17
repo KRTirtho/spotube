cd669e22c12679ae328fdefdb460cc16dc2e88d0
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-02-17 13:48:27 +0600

    website: new sveltekit based website (#1239)
    * feat: initialize website project
    
    * feat: add initial homepage with download links
    
    * feat: initial download page
    
    * fix: linux icon color
    
    * feat: add mobile nav and github star button
    
    * feat: add older and nightly downloads page
    
    * feat: add supporters and footer
    
    * feat: add author details in about page
    
    * feat: add darkmode toggle for website
    
    * feat: add playstore and flathub download buttons and contribution button
    
    * feat: add blogs support
    
    * feat: remove netlify deploy config and add cloudflare config and favicons + manifest
    
    * chore: add robots.txt
    
    * feat: add spotube logo in navbar and fix build errors
    
    * chore: add gap

---
+++ website/tests/test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test';
+
+test('index page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+});
