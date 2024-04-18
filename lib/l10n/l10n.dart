8354c0abce508639be2d33f42de9f7ed8466a5cf
Yusril Rapsanjani <yusriltakeuchi@gmail.com>
2024-04-18 16:27:21 +0700

    Add Indonesia translation

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -31,6 +31,7 @@ class L10n {
     const Locale('fr', 'FR'),
     const Locale('ne', 'NP'),
     const Locale('hi', 'IN'),
+    const Locale('id', 'ID'),
     const Locale('it', 'IT'),
     const Locale('ja', 'JP'),
     const Locale('ko', 'KR'),
