2b01e4fb4d816f98581ff3b6e2330008caa1273e
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-05-09 16:50:42 +0600

    chore: add translated message command to command list

--- cli/cli.dart
+++ cli/cli.dart
@@ -3,6 +3,7 @@ import 'package:args/command_runner.dart';
 import 'commands/build.dart';
 import 'commands/credits.dart';
 import 'commands/install-dependencies.dart';
+import 'commands/translated.dart';
 import 'commands/untranslated.dart';
 
 void main(List<String> args) {
@@ -14,6 +15,7 @@ void main(List<String> args) {
   commandRunner.addCommand(InstallDependenciesCommand());
   commandRunner.addCommand(BuildCommand());
   commandRunner.addCommand(CreditsCommand());
+  commandRunner.addCommand(TranslatedCommand());
   commandRunner.addCommand(UntranslatedCommand());
 
   commandRunner.run(args);
