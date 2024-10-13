6e1cd96903702724b8d7e39926de38992f906bdc
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-10-13 10:44:05 +0600

    website: fix older version download not working #1985

--- website/src/routes/downloads/older/+page.ts
+++ website/src/routes/downloads/older/+page.ts
@@ -1,14 +1,14 @@
-import type { PageLoad } from './$types';
-import { Octokit } from '@octokit/rest';
+import type { PageLoad } from "./$types";
+import { Octokit } from "@octokit/rest";
 
 const github = new Octokit();
 export const load: PageLoad = async () => {
 	const { data: releases } = await github.repos.listReleases({
-		owner: 'KRTirtho',
-		repo: 'spotube'
+		owner: "KRTirtho",
+		repo: "spotube",
 	});
 
 	return {
-		releases
+		releases,
 	};
 };
