ee7d0cfeb5b92bbaeae479eb28fca5bc59d5c91f
Kingkor Roy Tirtho <krtirtho@gmail.com>
2025-08-04 13:04:22 +0600

    website: markdown mdx support and wrap up other pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), mdx()]
 });
