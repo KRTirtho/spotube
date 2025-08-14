032309a2c61fdfd8279475b62bebd71f7cac4985
Kingkor Roy Tirtho <krtirtho@gmail.com>
2025-08-14 15:10:14 +0600

    feat: enhance markdown configuration and styling with rehype plugins and update global styles

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,16 +1,44 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      langAlias: {
+        hetu_script: "javascript",
+      },
+    },
+    gfm: true,
+    rehypePlugins: [
+      [rehypeSlug, {}],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap", // Adds the link at the end of the heading
+          properties: {
+            className: ["heading-link"], // Add a class for styling
+            "aria-hidden": "true",
+          },
+          content: {
+            // Optional: Use an SVG icon or text for the link
+            type: "element",
+            tagName: "span",
+            properties: { className: ["icon", "icon-link"] },
+            children: [{ type: "text", value: " #" }],
+          },
+        },
+      ],
+    ],
   },
   integrations: [react(), mdx()],
   redirects: {
